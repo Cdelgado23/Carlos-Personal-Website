6d3f3caf28282cbd38951c6b4855adafa2648c8e
Carlos Delgado <cdelgadoguiberteau@gmail.com>
2020-06-28 01:12:17 +0200

    add initial portfolio page

---
+++ scripts/portfolio.js
@@ -0,0 +1,36 @@
+var params = {
+    "currentIntent": {
+        "name": "Projects"
+    }
+}
+
+function LoadProjects() {
+    ajaxer(API_URL + "/projects", "POST", params, load_ok, log_cb);
+}
+function load_ok(response) {
+    console.log(response.dialogAction.message.content);
+    var parsed = parseData(response.dialogAction.message.content);
+    parsed.forEach(element => { if (element.CVContent != undefined) addEntry(formatEntry(element)) });
+}
+
+
+function formatEntry(project) {
+
+    console.log(project);
+    var html =
+        '<section class="nes-container is-rounded with-title separate-element" style="margin-top: 3%">\
+    <h4 class=" "> ' + project.CVContent.Title + '</h4>\
+    <span>' + project.CVContent.Description + '</span>\
+    <div class="separate-element">\
+    <a href="' + project.CVContent.Resource + '" class="nes-btn is-warning">Interview me </a>\
+    </div>\
+    </section>';
+
+    return html;
+}
+
+function addEntry(html) {
+    $("#entriesList").append(html);
+}
+
+LoadProjects();
